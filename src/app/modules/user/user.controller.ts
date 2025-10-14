b41453653570377f953b1d245a5204c4d472bd17
sajib689 <sajibbabu751@gmail.com>
2025-10-14 06:44:52 +0600

    updated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,14 +1,28 @@
 import type { Request, Response } from "express";
-
+import { User } from "./user.model.js";
+import httpStatus from "http-status-codes";
 
 const createUser = async (req: Request, res: Response) => {
-    try {
+  try {
+    const { name, email } = req.body;
 
-    } catch(error) {
-        if(error) {
-            res.status(500).json({
-                message: 'Error'
-            })
-        }
+    const user = await User.create({
+      name,
+      email,
+    });
+    res.status(httpStatus.CREATED).json({
+      message: "User Created Successfully",
+      user,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(httpStatus.BAD_REQUEST).json({
+        message: `Something Went Wrong ${error.message}`,
+      });
     }
-}
+  }
+};
+
+export const userController = {
+  createUser,
+};
