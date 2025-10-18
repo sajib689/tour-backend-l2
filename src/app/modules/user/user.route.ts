15ea4cd79b8c6668e400631f57bc2c6970aa60e5
sajib689 <jibonraja11@gmail.com>
2025-10-18 23:29:38 +0600

    d

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,7 +3,7 @@ import express from "express"
 import { userController } from "./user.controller.js"
 
 const userRouter = express.Router()
-
+// user routes
 userRouter.post("/register", userController.createUser)
 
 export default userRouter
