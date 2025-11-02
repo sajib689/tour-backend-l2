088dfbe69ae6051e7ea616d55d1c900e16f1ae26
sajib689 <sajibbabu751@gmail.com>
2025-11-02 20:27:36 +0600

    updated

--- src/app/middleware/userJwtVerify.ts
+++ src/app/middleware/userJwtVerify.ts
@@ -8,6 +8,7 @@ export const userJwtVerify =async (req: Request, res: Response, next: NextFuncti
         if(!accessToken){
           throw new AppError(401, "You are not authorized to access this route")
         }
+        
         const verifyToken =  jwt.verify(accessToken as string, process.env.JWT_TOKEN as string)
         console.log(verifyToken);
         if(!verifyToken){
