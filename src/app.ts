42b55aa8b0295a18c44ef24b50dbf5faeb837173
sajib689 <sajibbabu751@gmail.com>
2025-11-09 19:19:24 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,22 @@ import { router } from "./app/routes/index.js";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler.js";
 import { notFoundErrorHandler } from "./app/middleware/notFoundErrorHandler.js";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  expressSession({
+    secret: "",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use("/api/v1", router);
 
 app.use(globalErrorHandler);
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -10,6 +10,11 @@ interface EnvConfig {
   SUPER_ADMIN_PASSWORD: string;
   JWT_TOKEN: string;
   JWT_REFRESH_TOKEN: string;
+  GOOGLE_CLIENT_SECRET: string;
+  FRONTEND_URL: string;
+  Express_SESSION_SECRET: string;
+  GOOGLE_CALLBACK_URL: string;
+  GOOGLE_CLIENT_ID: string;
 }
 const loadEnvVariables = (): EnvConfig => {
   const requireEnvVariables: string[] = [
@@ -20,6 +25,11 @@ const loadEnvVariables = (): EnvConfig => {
     "SUPER_ADMIN_EMAIL",
     "JWT_TOKEN",
     "JWT_REFRESH_TOKEN",
+    "GOOGLE_CLIENT_SECRET",
+    "FRONTEND_URL",
+    "Express_SESSION_SECRET",
+    "GOOGLE_CALLBACK_URL",
+    "GOOGLE_CLIENT_ID",
   ];
   requireEnvVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -34,6 +44,11 @@ const loadEnvVariables = (): EnvConfig => {
     SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
     JWT_TOKEN: process.env.JWT_TOKEN as string,
     JWT_REFRESH_TOKEN: process.env.JWT_REFRESH_TOKEN as string,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+    Express_SESSION_SECRET: process.env.Express_SESSION_SECRET as string,
+    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL as string,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
   };
 };
 export const envVars: EnvConfig = loadEnvVariables();
